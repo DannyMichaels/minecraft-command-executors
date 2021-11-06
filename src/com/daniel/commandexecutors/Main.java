1ab11643cfe97e2057dd4ba033aa31f69a1250ec
Daniel <itzdanielmichael@gmail.com>
2021-11-06 15:43:27 -0400

    create minecraft command executors

---
+++ src/com/daniel/commandexecutors/Main.java
@@ -0,0 +1,15 @@
+package com.daniel.commandexecutors;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		System.out.println("MC COMMAND EXECUTORS PLUGIN ENABLED");
+
+		getCommand("heal").setExecutor(new HealCommand());
+		getCommand("time").setExecutor(new TimeCommand());
+
+	}
+}
---
+++ src/com/daniel/commandexecutors/TimeCommand.java
@@ -0,0 +1,26 @@
+package com.daniel.commandexecutors;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class TimeCommand implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		Calendar cal = Calendar.getInstance();
+		Player player = (Player) sender;
+
+		SimpleDateFormat dateFormat = new SimpleDateFormat("MM-dd-yyyy h:mm a"); // Output "2012-09-26 10:00 PM"
+		String formattedDateTime = dateFormat.format(cal.getTime());
+
+		player.sendMessage(ChatColor.RED + formattedDateTime);
+
+		return false;
+	}
+}
